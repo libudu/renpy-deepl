ae7158c1d4c59f8f627460f2318d06ca6aef3852
linpicio <3127966867@qq.com>
2021-08-09 02:08:26 +0800

    feat: 完成了提取正文

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // 获取源文本
 const fs = require('fs');
-const rowText = fs.readFileSync('./d0_after_free.rpy').toString().split('\r\n').filter(line => line);
+const rowString = fs.readFileSync('./d0_after_free.rpy').toString();
 
-// 按行拆分，过滤空行，去除首空
+// 处理文本，按行拆分，过滤空行，去除首空
+const rowText = rowString.split('\r\n').filter(line => line);
 const text = rowText.map(line => line.trim());
 
+// 判断每行文本的类型
 const typeTestList = [
   {
     type: '0原文位置',
@@ -33,7 +35,7 @@ function judgeType(line) {
 }
 const parseResult = text.map((line, index) => {
   const type = judgeType(line);
-  if(type === '原文') {
+  if(type === '2原文') {
     const start = line.indexOf('"');
     const trueText = line.slice(start + 1, line.length - 1);
     return {
@@ -50,4 +52,32 @@ const parseResult = text.map((line, index) => {
     text: line,
   };
 });
-console.log(parseResult);
+
+// 循环步骤，生成结果
+let step = null;
+const contentList = [];
+parseResult.forEach(result => {
+  const nextStep = Number(result.type[0]);
+  // 新的一句
+  if(nextStep == 0) {
+    step = 0;
+    return;
+  }
+  // 递进到下一句
+  if(nextStep == step + 1){
+    step += 1;
+    if(step == 2) {
+      contentList.push(result);
+    }
+    return;
+  }
+  // 没有成功递进到下一句，等待0开始下一句
+  step = null;
+  return;
+});
+
+// 提取有效句子，放入文本框
+const translateText = contentList.map(content => content.textContent).join('\r\n');
+fs.writeFileSync('./output.txt', translateText);
+
+// 将翻译结果还原为代码
